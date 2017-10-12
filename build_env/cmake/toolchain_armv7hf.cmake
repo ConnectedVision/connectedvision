0f2e5c49446f96d7eaeef9b59f1915f9effee9de
verbatom <32685909+verbatom@users.noreply.github.com>
2017-10-12 18:11:08 +0200

    fix Conan compiler.libcxx detection

--- build_env/cmake/toolchain_armv7hf.cmake
+++ build_env/cmake/toolchain_armv7hf.cmake
@@ -14,3 +14,9 @@ endif()
 set(CMAKE_CXX_COMPILER_VERSION ${CMAKE_MATCH_1})
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__STDC_CONSTANT_MACROS -DMACHINE_ARCH_ARM -fPIC -DLINUX=1 -UMMX")
+
+# indicate Conan to use C++11
+# this may be required for cross compiling on x86 when the standard GCC compiler is also installed and the Conan compiler detection overrules the compiler.libcxx setting defined in the Conan profile replacing libstdc++11 with libstdc++ 
+if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.1")
+	set(_GLIBCXX_USE_CXX11_ABI ON)
+endif()
